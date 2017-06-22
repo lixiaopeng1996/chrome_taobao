7116becb2b927edc79965a1661b9685935f38a44
杨国奇 <lnmput@gmail.com>
2017-06-22 23:04:34 +0800

    初步完成数据的获取

--- js/getTb.js
+++ js/getTb.js
@@ -1,7 +1,60 @@
-
-
 chrome.storage.local.get('product_name', function (result) {
     channels = result.product_name;
-
     $("#product-name").val(channels);
 });
+
+
+chrome.storage.local.get('product_list_price', function (result) {
+    channels = result.product_list_price;
+    $("#product-list-price").val(channels);
+});
+
+chrome.storage.local.get('product_price', function (result) {
+    channels = result.product_price;
+    $("#product-price").val(channels);
+});
+
+chrome.storage.local.get('product_source', function (result) {
+    channels = result.product_source;
+    $("#product-source").val(channels);
+});
+
+chrome.storage.local.get('product_store', function (result) {
+    channels = result.product_store;
+    $("#product-store").val(channels);
+});
+
+chrome.storage.local.get('product_option_colors', function (result) {
+    channels = result.product_option_colors;
+    var colorsBody = $("#colors");
+    $.each(channels, function (key,item) {
+        html = '<div class="form-group"><input type="text" value="'+item+'" class="form-control"></div>';
+        colorsBody.append(html);
+    });
+});
+
+chrome.storage.local.get('product_option_sizes', function (result) {
+    channels = result.product_option_sizes;
+    var sizesBody = $("#sizes");
+    $.each(channels, function (key,item) {
+        html = '<div class="form-group"><input type="text" value="'+item+'" class="form-control"></div>';
+        sizesBody.append(html);
+    });
+});
+
+chrome.storage.local.get('product_features', function (result) {
+    channels = result.product_features;
+    var featureBody = $("#feature");
+    $.each(channels, function (key,item) {
+        var fea = item.split(':');
+        html = '<div class="col-md-2"><div class="form-group"><input type="text" value="'+fea[0]+'" class="form-control"></div><div class="form-group"><input type="text" value="'+fea[1]+'" class="form-control"></div></div>';
+
+        featureBody.append(html);
+        console.log(fea);
+
+
+    });
+});
+
+
+
--- js/tb.js
+++ js/tb.js
@@ -6,16 +6,30 @@ $(function() {
     var tb_price = getPrice();
     var tb_source = getSource();
     var tb_store = getStore();
+    var tb_colors = getColors();
+    var tb_sizes = getSizes();
+    var tb_features = getFeatures();
+    var images = getImage();
 
 
-    console.log(tb_pname);
-    console.log(tb_list_price);
-    console.log(tb_price);
-    console.log(tb_source);
-    console.log(tb_store);
+    // console.log(tb_pname);
+    // console.log(tb_list_price);
+    // console.log(tb_price);
+    // console.log(tb_source);
+    // console.log(tb_store);
+    // console.log(tb_colors);
+    // console.log(tb_sizes);
+    // console.log(tb_features);
 
 
-
+    chrome.storage.local.set({'product_name': tb_pname});
+    chrome.storage.local.set({'product_list_price': tb_list_price});
+    chrome.storage.local.set({'product_price': tb_price});
+    chrome.storage.local.set({'product_source': tb_source});
+    chrome.storage.local.set({'product_store': tb_store});
+    chrome.storage.local.set({'product_option_colors': tb_colors});
+    chrome.storage.local.set({'product_option_sizes': tb_sizes});
+    chrome.storage.local.set({'product_features': tb_features});
 
 
 
@@ -42,6 +56,44 @@ $(function() {
         return $("#J_SpanStock").text();
     }
 
+    function getColors() {
+        var colors = new Array();
+        $(".J_TSaleProp.tb-img > li > a > span").each(function(){
+            colors.push($(this).text());
+        })
+        return colors;
+    }
+
+    function getSizes() {
+        var sizes =  new Array();
+        $(".J_TMySizeProp.J_Prop_measurement ul > li>a>span").each(function(){
+            sizes.push($(this).text());
+        })
+        return sizes;
+    }
+
+    function getFeatures() {
+        var features = new Array();
+        $(".attributes-list li").each(function () {
+            features.push($(this).text());
+        });
+        return features;
+    }
+
+    function getImage() {
+        var images = new Array();
+        $("img").each(function(){
+            var width = this.clientWidth;
+            var height = this.clientHeight;
+            if(width > 400 && height > 400 && this.src){
+                images.push(this.src);
+            }
+        });
+
+        console.log($.unique(images));
+
+
+    }
 
 });
 
