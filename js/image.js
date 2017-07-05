f774c12a88b5b7048a0524b846f075c49d270ba3
杨国奇 <lnmput@gmail.com>
2017-07-05 22:02:42 +0800

    完成图片裁剪

--- js/image.js
+++ js/image.js
@@ -5,7 +5,7 @@ $(function () {
         var thumbListBody = $("#thumb-list");
         $.each(channels, function (key,item) {
             //console.log(item);
-            var html = '<div class="row"><img src="'+item+'"></div>';
+            var html = '<div class="row"><img crossOrigin="Anonymous" src="'+item+'"></div>';
             thumbListBody.append(html);
 
         });
@@ -16,7 +16,7 @@ $(function () {
         var channels = result.product_images;
         var imageListBody = $("#image-list");
         $.each(channels, function (key,item) {
-            var html = '<div class="row"><img src="'+item+'"></div>';
+            var html = '<div class="row"><img crossOrigin="Anonymous" src="'+item+'"></div>';
             imageListBody.append(html);
 
         });
@@ -31,31 +31,86 @@ $(function () {
         $("#image-container").css("display","none");
     });
 
-    $(document.body).on('click', '#thumb-list img', function(event) {
+    $(document.body).on('click', '.col-md-7 img', function(event) {
         var width = this.clientWidth;
         var height = this.clientHeight;
         $(this).parent().css('width', width);
         $(".current-image-width").html(width);
         $(".current-image-height").html(height);
         $("#current-image").attr("src", $(this).attr("src"));
-        $(this).cropper({
+        $image = this;
+        //new Cropper
+        cropper = new Cropper($image, {
             aspectRatio: 4 / 3,
             zoomable: false,
             rotatable : false,
             autoCropArea: 0.8,
             width: 100,
             height: 100,
+
             crop: function(e) {
                 // Output the result data for cropping image.
-                $(".select-x").html(e.x);
-                $(".select-y").html(e.y);
-                $(".select-w").html(e.width);
-                $(".select-h").html(e.height);
+                $(".select-x").html(e.detail.x);
+                $(".select-y").html(e.detail.y);
+                $(".select-w").html(e.detail.width);
+                $(".select-h").html(e.detail.height);
             }
         });
+
+
+
+
+        
+
     });
 
-    $("#ok").click(function () {
+
+
+    document.getElementById('ok').addEventListener('click', function(){
+        var src = $("#current-image").attr("src");
+        var x = $(".select-x").text();
+        var y = $(".select-y").text();
+        var w = $(".select-w").text();
+        var h = $(".select-h").text();
+
+        var height = $(".current-image-width").text();
+        var width = $(".current-image-height").text();
+
+        console.log(src);
+        console.log(x);
+        console.log(y);
+        console.log(w);
+        console.log(h);
+        console.log(width);
+        console.log(height);
+
+        var imgurl =  cropper.getCroppedCanvas().toDataURL();
+        var img = document.createElement("img");
+        img.src = imgurl;
+
+        var html = '<div class="col-md-4"><img crossOrigin="Anonymous" src="'+imgurl+'" class="img-rounded"></div>';
+        $("#image-select-content").append(html);
+       //
+       // document.getElementById("mage-select-content").append(img);
+
+
+
+
+    });
+
+    $("#okc").click(function () {
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
