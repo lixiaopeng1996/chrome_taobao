b49808ac34994b90322f4ef217495d8acdae859b
杨国奇 <lnmput@gmail.com>
2017-07-11 00:06:40 +0800

    开始上传图片

--- js/image.js
+++ js/image.js
@@ -31,7 +31,22 @@ $(function () {
         $("#image-container").css("display","none");
     });
 
+
+    chrome.storage.local.get('site', function (result) {
+        var channels = result.site;
+        if (Cookies.get('api_image')) {
+            Cookies.remove('api_image');
+        }
+        Cookies.set('api_image', channels.site_api_image);
+    });
+
+    api_image = Cookies.get('api_image');
+
     cropperObj = [];
+    optionImages = [];
+    thumbImages = [];
+    featureImages = [];
+
     $(document.body).on('click', '.col-md-7 img', function(event) {
         var width = this.clientWidth;
         var height = this.clientHeight;
@@ -42,7 +57,6 @@ $(function () {
         $("#current-image").attr("src", src);
         $image = this;
         //new Cropper
-
         cropper = new Cropper($image, {
             aspectRatio: 4 / 3,
             zoomable: false,
@@ -60,14 +74,8 @@ $(function () {
 
             }
         });
-
         cropperObj[src] = cropper;
-
-        console.log(cropperObj);
-
     });
-
-
 
     document.getElementById('ok').addEventListener('click', function(){
         var src = $("#current-image").attr("src");
@@ -79,23 +87,33 @@ $(function () {
         var height = $(".current-image-width").text();
         var width = $(".current-image-height").text();
 
-        console.log(src);
-        console.log(x);
-        console.log(y);
-        console.log(w);
-        console.log(h);
-        console.log(width);
-        console.log(height);
+        // console.log(src);
+        // console.log(x);
+        // console.log(y);
+        // console.log(w);
+        // console.log(h);
+        // console.log(width);
+        // console.log(height);
         var imgurl =  cropperObj[src].getCroppedCanvas().toDataURL();
         var img = document.createElement("img");
         img.src = imgurl;
-        var html = '<div class="col-md-4"><img crossOrigin="Anonymous" src="'+imgurl+'" class="img-rounded"><span class="glyphicon glyphicon-remove-circle remove-crop-image"></span></div>';
+
+        var thumbImagesSrc = api_image+'?key=fa94h9a2y5bv43n2zg3s&url='+src+'&dw=600&dh=800&x='+x+'&y='+y+'&w='+w+'&h='+h+'&id=840&typeid=1';
+        var featureImagesSrc = api_image+'?key=fa94h9a2y5bv43n2zg3s&url='+src+'&dw=600&dh=800&x='+x+'&y='+y+'&w='+w+'&h='+h+'&id=840&typeid=2';
+
+        var noneSrc = '<img style="display: none;" class="thumbImages" data-src="'+thumbImagesSrc+'"><img style="display: none;" class="featureImages" data-src="'+featureImagesSrc+'">';
+
+        var html = '<div class="col-md-4">'+noneSrc+'<img crossOrigin="Anonymous" src="'+imgurl+'" class="img-rounded"><span class="glyphicon glyphicon-remove-circle remove-crop-image"></span></div>';
         $("#image-select-content").append(html);
+
 
 
         // 组装图片提交信息
 
-        
+
+
+
+
     });
 
 
@@ -107,30 +125,58 @@ $(function () {
     $("input[name='image-type']").change(function () {
         var type = $(this).val();
         if (type == 'option') {
+            $(".option-list").css("display", "block");
 
 
-            alert(type);
+
+
+
+
+            // optionImages[] = api_image+'?key=fa94h9a2y5bv43n2zg3s&url='+src+'&dw=600&dh=800&x='+x+'&y='+y+'&w='+w+'&h='+h+'&id=840&typeid=1&optionValue=红色';
+        } else {
+            $(".option-list").css("display", "none");
         }
 
 
     });
 
 
-
-
-
-
     $("#upload-image").click(function () {
         var  imageType =  $("input[name='image-type']:checked").val();
+        if (imageType == 'thumb') {
+            var thumbImages = $(".thumbImages");
+            $(".thumbImages").each(function () {
+                var src = $(this).data('src');
 
-        alert(imageType);
+                console.log(src);
+            });
+
+
+        } else if (imageType == 'feature') {
+            var featureImages = $(".featureImages");
+            $(".featureImages").each(function () {
+                var src = $(this).data('src');
+
+                console.log(src);
+            });
+
+        } else {
+
+
+
+
+            alert('123');
+        }
+
+
+
 
 
 
     });
 
 
-    $("#image-container").css("display","none");
+    //$("#image-container").css("display","none");
 });
 
 
